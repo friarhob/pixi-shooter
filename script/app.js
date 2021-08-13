f5ba61e85f0acef842fe9e42fff8b14e26101be7
Friar Hob <github@friarhob.33mail.com>
2021-08-13 15:40:28 -0300

    Creating enemy ships

--- script/app.js
+++ script/app.js
@@ -2,6 +2,7 @@ let spaceshipColour = 0x1b71f2;
 let backgroundColour = 0x001028;
 let scoreBoardColour = 0xc2131f;
 let bulletColour = 0xc90411;
+let enemyColour = 0x2ef262;
 
 let app = new PIXI.Application({
     width: 100,
@@ -29,6 +30,7 @@ spaceship.velocity = {};
 spaceship.velocity.y = 0;
 
 let shoots = [];
+let enemies = [];
 
 document.addEventListener("keydown", (event) => {
     if (event.key === "ArrowUp") spaceship.velocity.y = -1;
@@ -53,18 +55,46 @@ document.addEventListener("keyup", (event) => {
         spaceship.velocity.y = 0;
 });
 
+let nTicks = 0;
+
 app.ticker.add((delta) => {
+    nTicks += 1;
+    
+    /* Moving spaceship */
     spaceship.y += spaceship.velocity.y * window.innerHeight * 0.01;
     if (spaceship.y < 0) spaceship.y = 0;
     if (spaceship.y > window.innerHeight - spaceship.height)
         spaceship.y = window.innerHeight - spaceship.height;
 
+    /* Updating shoots */
     for (const shoot in shoots) {
         shoots[shoot].x += window.innerWidth * 0.015;
         shoots[shoot].rotation += 0.15;
         if (shoots[shoot].x > window.innerWidth) {
             shoots[shoot].destroy();
             shoots.splice(shoot, 1);
+        }
+    }
+
+    /* Creating new enemies */
+    if(nTicks % 80 == 0)
+    {
+        let enemy = PIXI.Sprite.from("../images/starfighter.png");
+        app.stage.addChild(enemy);
+        enemies.push(enemy);
+        enemy.width = spaceship.width;
+        enemy.height = spaceship.height;
+        enemy.x = window.innerWidth;
+        enemy.y = Math.random()*window.innerHeight;
+    }
+
+    /* Moving enemies */
+    for(const enemy in enemies) {
+        enemies[enemy].x -= window.innerWidth*0.005;
+        if(enemies[enemy].x <= 0) {
+            /* TODO: make game over */
+            enemies[enemy].destroy();
+            enemies.splice(enemy, 1);
         }
     }
 });
